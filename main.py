c46d21cc15e1c0a618ffbde822404f806e2e6816
lheywang <90984325+lheywang@users.noreply.github.com>
2024-06-13 14:58:07 +0200

    Added a first test on the database management

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+This is the entry point of the program.
+"""
+
+from Database import DataBase
+
+
+def main():
+    print("Hello Worlds !")
+
+    DB = DataBase()
+    del DB
+
+
+if __name__ == "__main__":
+    main()
