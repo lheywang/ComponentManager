a1c455c34420d6fa02baba42076fcd1301646628
lheywang <90984325+lheywang@users.noreply.github.com>
2024-06-22 21:23:12 +0200

    Moved the project to a virtualenv

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ComponentManager
 
 Store your components and manage their stock automatically :)
+
+## Required modules : 
+- Pandas
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ This is the entry point of the program.
 
 from Database import DataBase
 
+import pandas as pd
+
 
 def main():
     print("Hello Worlds !")
@@ -18,4 +20,6 @@ def main():
 
 
 if __name__ == "__main__":
+    data = pd.read_csv("PCBA_3128D2_4.csv")
+    print(data)
     main()
